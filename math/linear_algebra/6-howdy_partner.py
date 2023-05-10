2a72b2bec95dcfb13dc1353d1bff650a4e9a0e78
spindouken <mlc9181209@gmail.com>
2023-05-10 17:22:53 -0500

    docstrings and compliance

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+"""Concatenates two arrays and returns the resulting array"""
+
+
 def cat_arrays(arr1, arr2):
+    """takes two input arrays (lists of integers/floats) and concatenates
+    them, creating a new array containing the elements of both input arrays"""
     return arr1 + arr2
