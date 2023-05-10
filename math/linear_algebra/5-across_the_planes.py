6feb58a13605313dd198d44f7ec0a8a0027e9202
spindouken <mlc9181209@gmail.com>
2023-05-10 17:04:45 -0500

    pycode compliance

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -10,4 +10,6 @@ def add_matrices2D(mat1, mat2):
     number of rows and columns)"""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    return [[mat1[rowIndex][columnIndex] + mat2[rowIndex][columnIndex] for columnIndex in range(len(mat1[0]))] for rowIndex in range(len(mat1))]
+    return [[mat1[rowIndex][columnIndex] + mat2[rowIndex][columnIndex]
+            for columnIndex in range(len(mat1[0]))]
+            for rowIndex in range(len(mat1))]
