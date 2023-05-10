abaf41e559639a1a939917cc7b9ed33e479dd532
spindouken <mlc9181209@gmail.com>
2023-05-10 16:44:24 -0500

    pycode compliance

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Find the shape of a matrix and return the dimensions as a list of integers"""
+"""Find the shape of a matrix and
+return the dimensions as a list of integers"""
+
 
 def matrix_shape(matrix):
     """Returns the dimensions of a given matrix"""
