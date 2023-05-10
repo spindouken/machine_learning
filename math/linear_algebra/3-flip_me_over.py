f9f08f67ce2bbf04316a174631ed591850c9f3b3
spindouken <mlc9181209@gmail.com>
2023-05-10 16:49:31 -0500

    documented and pycode compliant

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+"""Transpose a given matrix"""
+
+
 def matrix_transpose(matrix):
-    return [[matrix[columnIndex][rowIndex] for columnIndex in range(len(matrix))] for rowIndex in range(len(matrix[0]))]
+    """
+    Takes a 2D matrix as input and returns the transposed matrix.
+    This is done by swapping the rows and colums.
+    """
+    transposedMatrix = [[matrix[columnIndex][rowIndex]
+                        for columnIndex in range(len(matrix))]
+                        for rowIndex in range(len(matrix[0]))]
+    return transposedMatrix
