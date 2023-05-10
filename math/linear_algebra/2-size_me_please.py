cf1c97c64c860b90d4e52fda0784065a8bdfc39d
spindouken <mlc9181209@gmail.com>
2023-05-10 16:39:49 -0500

    docstring addition

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
+    """Returns the dimensions of a given matrix"""
     matrixDimensions = []
     while isinstance(matrix, list):
         matrixDimensions.append(len(matrix))
