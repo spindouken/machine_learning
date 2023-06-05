573aebb3ab740f84ababaadf9bdda275bbcf3e6b
spindouken <mlc9181209@gmail.com>
2023-06-05 11:00:20 -0500

    docstrings addition

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
-"""placeholder"""
+"""creating nodes in a network.... networking....a neural network"""
 import numpy as np
 
 
 class NeuralNetwork:
-    """placeholder"""
+    """
+    defines a neural network with one hidden layer.
+    performing binary classification.
+    """
     def __init__(self, nx, nodes):
-        """placeholder"""
+        """
+        initialize a neural network
+        nx: the number of input features
+        nodes: the number of nodes found in the hidden layer
+        W1: The weights vector for the hidden layer.
+            initialized using a random normal distribution.
+        b1: The bias for the hidden layer. Initialized with 0’s.
+        A1: The activated output for the hidden layer. Initialized to 0.
+        W2: The weights vector for the output neuron.
+            Initialized using a random normal distribution.
+        b2: The bias for the output neuron. Initialized to 0.
+        A2: The activated output for the output neuron (prediction).
+            Initialized to 0.
+        """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
