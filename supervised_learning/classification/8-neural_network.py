6fee84587eea493aeff7dc832e178d975c7f2e69
spindouken <mlc9181209@gmail.com>
2023-06-05 10:50:41 -0500

    random normal distribution syntax fix

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -15,9 +15,9 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        self.W1 = np.random.normal(size=(nodes, nx))
+        self.W1 = np.random.randn(nodes, nx)
         self.b1 = 0
         self.A1 = 0
-        self.W2 = np.random.normal(size=(1, nodes))
+        self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
