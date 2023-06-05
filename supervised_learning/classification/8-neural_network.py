3a8c544db4f90513be0a403cbefcf85dd8e2f00e
spindouken <mlc9181209@gmail.com>
2023-06-05 10:52:54 -0500

    better initialization?

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -16,7 +16,7 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = 0
+        self.b1 = np.zeros(shape=(nodes, 1))
         self.A1 = 0
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
