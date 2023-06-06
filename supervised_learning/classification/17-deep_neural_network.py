4ad297ae48a38e6d64b3808076a092a5fb87d171
spindouken <mlc9181209@gmail.com>
2023-06-06 15:06:41 -0500

    task 17

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""are you winning at neural networks son?"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    defines a deep neural network performing binary classification
+    """
+    def __init__(self, nx, layers):
+        """
+        L: number of layers in the neural network
+        cache: A dictionary to hold all intermediary values of the network.
+            Upon instantiation, it is set to an empty dictionary
+        weights: A dictionary to hold all weights and biases of the network
+            Upon instantiation,
+            weights initialized using He et al. method
+                and saved in the weights dictionary using the key W{1}
+                    where {1} is the hidden layer the weight belongs to
+            biases initialized to 0's and saved
+                in the weights dictionary using the key b{1}
+                    where {1} is the hiddne layer the bias belongs to
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__nx = nx
+        self.__layers = layers
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(self.__L):
+            if i == 0:
+                self.__weights['W' + str(i+1)] = np.random.randn(layers[i], nx) \
+                    * np.sqrt(2/nx)
+            else:
+                self.__weights['W' + str(i+1)] = np.random. \
+                    randn(layers[i], layers[i-1]) * np.sqrt(2/layers[i-1])
+            self.__weights['b' + str(i+1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
