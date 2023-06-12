e424cea3c31ce7321db30b4d5bc41aad0d0c66ee
spindouken <mlc9181209@gmail.com>
2023-06-12 18:59:34 -0500

    task 2

---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""forward propagation function"""
+import tensorflow as tf
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """
+    creates the forward propagation graph for the neural network
+    x is the placeholder for the input data
+    layer_sizes: list containing the number of nodes
+        in each layer of the network
+    activations: list containing the activation functions
+        for each layer of the network
+    Returns the prediction of the network in tensor form
+    """
+    create_layer = __import__('1-create_layer').create_layer
+    output = x
+    for i in range(len(layer_sizes)):
+        output = create_layer(output, layer_sizes[i], activations[i])
+        prediction = output(output)
+    return prediction
