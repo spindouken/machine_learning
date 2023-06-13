7bf7085c7354aaf8324f5badc7b447121c6bbd9b
spindouken <mlc9181209@gmail.com>
2023-06-12 19:01:43 -0500

    syntax issue fix

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -17,5 +17,5 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     output = x
     for i in range(len(layer_sizes)):
         output = create_layer(output, layer_sizes[i], activations[i])
-        prediction = output(output)
+        prediction = output
     return prediction
