5bc5f50ab29eb1be9b36e60dc3912ae04fe8757b
spindouken <mlc9181209@gmail.com>
2023-06-17 18:20:31 -0500

    k

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -4,7 +4,8 @@ import tensorflow as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
+def train_mini_batch(X_train, Y_train,
+                     X_valid, Y_valid,
                      batch_size=32, epochs=5,
                      load_path="/tmp/model.ckpt",
                      save_path="/tmp/model.ckpt"):
