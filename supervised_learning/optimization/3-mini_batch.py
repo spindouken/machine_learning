144e1765138dcea288b4f64a3d9fe95e1d63cbd8
spindouken <mlc9181209@gmail.com>
2023-06-17 19:01:55 -0500

    remove comments test

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -11,21 +11,17 @@ def train_mini_batch(X_train, Y_train,
                      save_path="/tmp/model.ckpt"):
     """checker bug test"""
     with tf.Session() as sess:
-        # 1) import meta graph and restore session
         saver = tf.train.import_meta_graph("{}.meta".format(load_path))
         saver.restore(sess, load_path)
 
-        # 2) Get the following tensors and ops from the collection
         x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
         train_op = tf.get_collection("train_op")[0]
 
-        # calculate number of iterations per epoch
         m = X_train.shape[0]
 
-        # calculate and print cost and accuracy before the first epoch
         train_cost = sess.run(loss, feed_dict={x: X_train, y: Y_train})
         train_accuracy = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
         valid_cost = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
@@ -36,18 +32,12 @@ def train_mini_batch(X_train, Y_train,
         print("\tValidation Cost: {}".format(valid_cost))
         print("\tValidation Accuracy: {}".format(valid_accuracy))
 
-        # 3) loop over epochs
         for epoch in range(epochs):
-            # shuffle data
             X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
-            # loop over the batches
             for i in range(0, m, batch_size):
-                # extract batches
                 X_batch = X_shuffled[i:i + batch_size]
                 Y_batch = Y_shuffled[i:i + batch_size]
-                # train model
                 sess.run(train_op, feed_dict={x: X_batch, y: Y_batch})
-                # print step cost and accuracy after every 100 steps
                 if (i // batch_size) % 100 == 0:
                     cost = sess.run(loss, feed_dict={x: X_batch, y: Y_batch})
                     acc = sess.run(accuracy,
@@ -56,7 +46,6 @@ def train_mini_batch(X_train, Y_train,
                     print("\t\tCost: {}".format(cost))
                     print("\t\tAccuracy: {}".format(acc))
 
-            # print training and validation cost after each epoch
             train_cost = sess.run(loss, feed_dict={x: X_train, y: Y_train})
             train_accuracy = sess.run(
                 accuracy, feed_dict={x: X_train, y: Y_train}
@@ -70,6 +59,5 @@ def train_mini_batch(X_train, Y_train,
             print("\tTraining Accuracy: {}".format(train_accuracy))
             print("\tValidation Cost: {}".format(valid_cost))
             print("\tValidation Accuracy: {}".format(valid_accuracy))
-        # 4) save session
         save_path = saver.save(sess, save_path)
     return save_path
