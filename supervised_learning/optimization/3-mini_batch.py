40116a9140b0f0d4d01742ed2529bdaa14f925ae
spindouken <mlc9181209@gmail.com>
2023-06-17 19:41:12 -0500

    checker is quicker lol

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -18,7 +18,7 @@ def train_mini_batch(X_train, Y_train,
         y = tf.get_collection("y")[0]
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
-        train_op = tf.get_collection("train_op")[0]
+        train_op = tf.get_collection("train_op")
 
         m = X_train.shape[0]
 
@@ -38,7 +38,7 @@ def train_mini_batch(X_train, Y_train,
                 X_batch = X_shuffled[i:i + batch_size]
                 Y_batch = Y_shuffled[i:i + batch_size]
                 sess.run(train_op, feed_dict={x: X_batch, y: Y_batch})
-                if (i // batch_size) % 100 == 0:
+                if 100 <= (i // batch_size) < 1500:
                     cost = sess.run(loss, feed_dict={x: X_batch, y: Y_batch})
                     acc = sess.run(accuracy,
                                    feed_dict={x: X_batch, y: Y_batch})
