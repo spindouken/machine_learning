40423221bf206a83599e8cdc4a90020e47cd820e
spindouken <mlc9181209@gmail.com>
2023-06-20 12:59:06 -0500

    import statement

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """function that creates the training operation for a neural network in
 tensorflow using the gradient descent with momentum optimization algorithm"""
+import tensorflow as tf
 
 
 def create_momentum_op(loss, alpha, beta1):
