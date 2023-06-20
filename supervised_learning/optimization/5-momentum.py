fa85d8340e05d9ba4ba25c6f9740c47b29240557
spindouken <mlc9181209@gmail.com>
2023-06-20 12:51:17 -0500

    added documentation

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -4,6 +4,14 @@ the gradient descent with momentum optimization algorithm"""
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    alpha: learning rate
+    beta1: momentum weight
+    var: numpy.ndarray containing the variable to be updated
+    grad: numpy.ndarray containing the gradient of var
+    v: previous first moment of var
+    Returns the updated variable and the new moment, respectively
+    """
     v = beta1 * v + (1 - beta1) * grad
     var = var - alpha * v
     return var, v
