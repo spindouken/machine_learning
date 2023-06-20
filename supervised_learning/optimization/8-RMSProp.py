28bd5df2c0c278df20143b2d79d59cc27c005dd2
spindouken <mlc9181209@gmail.com>
2023-06-20 15:42:01 -0500

    task 8

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+creates the training operation for a neural network in tensorflow
+using the RMSProp optimization algorithm
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    loss: loss of the network
+    alpha: learning rate
+    beta2: RMSProp weight
+    epsilon: small number to avoid division by zero
+    Returns the RMSProp optimization operation
+    """
+    optimizer = tf.train.RMSPropOptimizer(alpha, decay=beta2, epsilon)
+    train_op = optimizer.minimize(loss)
+    return train_op
