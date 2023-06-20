634452b881386f63217303883309529515c46159
spindouken <mlc9181209@gmail.com>
2023-06-20 12:57:58 -0500

    task 6

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""function that creates the training operation for a neural network in
+tensorflow using the gradient descent with momentum optimization algorithm"""
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    loss: loss of the network
+    alpha: learning rate
+    beta1: momentum weight
+    Returns the momentum optimization operation
+    """
+    optimizer = tf.train.MomentumOptimizer(learning_rate=alpha, momentum=beta1)
+    train_op = optimizer.minimize(loss)
