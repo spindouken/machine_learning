283079c45c5c88078f5e411c2812f39193d1e164
spindouken <mlc9181209@gmail.com>
2023-06-25 15:04:40 -0500

    proto files

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""creates a confusion matrix"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    creates a confusion matrix
+    labels: one-hot numpy.ndarray of shape (m, classes)
+        containing the correct labels for each data point
+        m: number of data points
+        classes: number of classes
+    logits: one-hot numpy.ndarray of shape (m, classes)
+        containing the predicted labels
+    Returns a confusion numpy.ndarray of shape (classes, classes)
+    """
+
---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+calculates the sensitivity for each class in a confusion matrix:
+"""
+import numpy as np
+
+
+def sensitivity(confusion):
+    """
+    confusion: (numpy.ndarray) confusion matrix
+    of shape (classes, classes)
+        row indices: re
+        classes: number of classes
+    Returns numpy.ndarray: array of shape (classes,) containing the sensitivity of each class
+    """
+
