50d47980cfbfe2f12182e41bb0029bb8094dcb0f
spindouken <mlc9181209@gmail.com>
2023-06-27 17:44:45 -0500

    descriptions!

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -15,24 +15,31 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     weights: dictionary of the weights and biases of the neural network
     cache: dictionary of the outputs of each layer of the neural network
     alpha: learning rate
-    lambtha: the L2 regularization parameter
+    lambtha: L2 regularization parameter
     L: number of layers of the network
     Notes: network uses tanh activations on each layer except the last
         ...last layer uses a softmax activation
     Updates the weights and biases of the network using gradient descent
     """
-    m = Y.shape[1]
-    dZ = cache['A' + str(L)] - Y
+    m = Y.shape[1]  # Number of data points
+    dZ = cache['A' + str(L)] - Y  # Calculate the error derivative
 
+    # Loop over each layer in reverse order (from output to input)
     for layerIndex in range(L, 0, -1):
-        activation_of_prev_layer = cache['A' + str(layerIndex - 1)]
-        dW = np.matmul(
-            dZ, activation_of_prev_layer.T
-            ) / m + (lambtha / m) * weights['W' + str(layerIndex)]
+        A = cache['A' + str(layerIndex - 1)]  # Activation of previous layer
+
+        # Calculate the weight derivative (including L2 regularization term)
+        dW = np.matmul(dZ, A.T) / m + (lambtha / m) * weights[
+            'W' + str(layerIndex)
+            ]
+
+        # Calculate the bias derivative
         db = np.sum(dZ, axis=1, keepdims=True) / m
+
+        # If not the first layer, calculate dZ for the next layer
         if layerIndex > 1:
-            dZ = np.matmul(
-                weights['W' + str(layerIndex)].T, dZ
-                ) * (1 - activation_of_prev_layer ** 2)
+            dZ = np.matmul(weights['W' + str(layerIndex)].T, dZ) * (1 - A ** 2)
+
+        # Update weights and biases
         weights['W' + str(layerIndex)] -= alpha * dW
         weights['b' + str(layerIndex)] -= alpha * db
