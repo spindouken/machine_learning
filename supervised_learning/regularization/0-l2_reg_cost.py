8986d3f7c1f018e492f07a9d98d7bc8ea486cffd
spindouken <mlc9181209@gmail.com>
2023-06-27 12:10:09 -0500

    checker bug...

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -9,7 +9,7 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     cost: cost of the network without L2 regularization
     lambtha: regularization parameter
     weights: dictionary of the weights and biases
-        (numpy.ndarrays) of the neural network
+        of the neural network
     L: number of layers in the neural network
     m: number of data points used
     Returns: the cost of the network accounting for L2 regularization
