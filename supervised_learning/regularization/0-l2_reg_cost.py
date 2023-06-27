9be6698161648eea92235e9fa4096797d89c938d
spindouken <mlc9181209@gmail.com>
2023-06-27 17:49:11 -0500

    descriptions!

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -13,18 +13,24 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
         (numpy.ndarrays) of the neural network
     L: number of layers in the neural network
     m: number of data points used
-    Returns the cost of the network accounting for L2 regularization
+    Returns: the cost of the network accounting for L2 regularization
+    The L2 regularization cost is calculated using the formula:
+    Jreg = cost + (lambtha / (2 * m)) * sum(weights^2)
     """
-    sum_of_squared_weights = 0
+    sum_of_squared_weights = 0  # Initialize the sum of squared weights
 
-    # calculate the sum of squared weights for each layer
+    # Loop over each layer
     for layer_index in range(1, L + 1):
+        # Add the squared norm (sum of squares of each element)
+        # of the weight matrix for this layer
         sum_of_squared_weights += np.linalg.norm(
             weights['W' + str(layer_index)]
             ) ** 2
 
+    # Calculate the regularization term
     regularization_term = lambtha / (2 * m) * sum_of_squared_weights
 
+    # Add the regularization term to the original cost
     Jreg = cost + regularization_term
 
-    return Jreg
+    return Jreg  # Return the cost with L2 regularization
