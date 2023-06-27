4c77cd2c7a4bc36f56906cd630016946869c0bd7
spindouken <mlc9181209@gmail.com>
2023-06-27 12:20:50 -0500

    task 2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -2,6 +2,7 @@
 """
  calculates the cost of a neural network with L2 regularization
 """
+import tensorflow as tf
 
 
 def l2_reg_cost(cost):
@@ -11,3 +12,7 @@ def l2_reg_cost(cost):
     Returns a tensor containing the cost
         of the network accounting for L2 regularization
     """
+    regularizer = tf.contrib.layers.l2_regularizer(lambtha)
+    weights = tf.trainable_variables()
+    l2_reg_cost = cost + tf.contrib.layers.apply_regularization(regularizer, weights)
+    return l2_reg_cost
