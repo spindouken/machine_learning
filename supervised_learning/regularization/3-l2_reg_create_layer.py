824211ce85ededf32b5234b54977873d03bd626c
spindouken <mlc9181209@gmail.com>
2023-06-27 12:27:54 -0500

    task 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -2,6 +2,7 @@
 """
 creates a tensorflow layer that includes L2 regularization
 """
+import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
@@ -11,6 +12,12 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     n: number of nodes the new layer should contain
     activation: the activation function that should be used on the layer
     lambtha: L2 regularization parameter
-    Returns: output of the new layer
+    Returns the output of the new layer
     """
-
+    regularization = tf.contrib.layers.l2_regularizer(regularization_parameter)
+    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    l2_regularized_layer = tf.layers.Dense(number_of_nodes,
+                                           activation=activation_function,
+                                           kernel_regularizer=regularization,
+                                           kernel_initializer=initializer)
+    return l2_regularized_layer(previous_layer)
