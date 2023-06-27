0e283385ef2d82be9bebf0a59047266c9cb842c9
spindouken <mlc9181209@gmail.com>
2023-06-27 12:06:54 -0500

    task 0

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -8,9 +8,22 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """
     cost: cost of the network without L2 regularization
     lambtha: regularization parameter
-    weights: dictionary of the weights and biases (numpy.ndarrays) of the neural network
+    weights: dictionary of the weights and biases
+        (numpy.ndarrays) of the neural network
     L: number of layers in the neural network
     m: number of data points used
     Returns: the cost of the network accounting for L2 regularization
     """
+    sum_of_squared_weights = 0
 
+    # calculate the sum of squared weights for each layer
+    for layer_index in range(1, L + 1):
+        sum_of_squared_weights += np.linalg.norm(
+            weights['W' + str(layer_index)]
+            ) ** 2
+
+    regularization_term = lambtha / (2 * m) * sum_of_squared_weights
+
+    cost_with_l2_regularization = cost + regularization_term
+
+    return cost_with_l2_regularization
