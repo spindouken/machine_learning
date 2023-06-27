bb344a1fd91e80e0facb0eb854ba9afd80f8e55e
spindouken <mlc9181209@gmail.com>
2023-06-27 12:11:56 -0500

    forgot import

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -2,11 +2,18 @@
 """
 calculates the cost of a neural network with L2 regularization
 """
+import numpy as np
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """
-    placeholder
+    cost: cost of the network without L2 regularization
+    lambtha: regularization parameter
+    weights: dictionary of the weights and biases
+        (numpy.ndarrays) of the neural network
+    L: number of layers in the neural network
+    m: number of data points used
+    Returns: the cost of the network accounting for L2 regularization
     """
     sum_of_squared_weights = 0
 
--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -8,5 +8,6 @@ def l2_reg_cost(cost):
     """
     calculates the cost of a neural network with L2 regularization
     cost: tensor containing the cost of the network without L2 regularization
-    Returns: tensor containing the cost of the network accounting for L2 regularization
+    Returns a tensor containing the cost
+        of the network accounting for L2 regularization
     """
