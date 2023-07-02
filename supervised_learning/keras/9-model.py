976bf05bae7abf2bb616b71b665cfac392dbf77a
spindouken <mlc9181209@gmail.com>
2023-07-02 17:33:15 -0500

    task 9

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-"""save and load model functions"""
+"""save and load model functions in keras"""
+import os.path
+from tensorflow.keras.models import load_model
 
 
 def save_model(network, filename):
     """
     network is the model to save
     filename is the path of the file that the model should be saved to
+    save function saves:
+        architecture of the model, allowing it to re-recreate the model
+        the weights of the model
+        the training configuration (loss, optimizer)
+        the state of the optimizer, allowing to resume training
+            from exactly where you left off
     Returns: None
     """
+    if os.path.isfile(filename) is False:
+        model.save(filename)
+
 
 def load_model(filename):
     """
     filename is the path of the file that the model should be loaded from
     Returns: the loaded model
     """
-
+    model = load_model(filename)
+    return model
