068d8ce9612e123dfd4c64ccfbd25e9e50d6a994
spindouken <mlc9181209@gmail.com>
2023-07-03 15:47:56 -0500

    task 1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 """builds a neural network with the Keras library"""
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     nx is the number of input features to the network
-    layers is a list containing the number of nodes in each layer of the network
-    activations is a list containing the activation functions used for each layer of the network
+    layers is a list containing the number of nodes
+        in each layer of the network
+    activations is a list containing the activation functions
+        used for each layer of the network
     lambtha is the L2 regularization parameter
     keep_prob is the probability that a node will be kept for dropout
     You are not allowed to use the Sequential class
     Returns: the keras model
     """
-    
+    inputs = K.Input(shape=(nx,))
+    x = inputs
+    for i in range(len(layers)):
+        x = K.layers.Dense(
+            layers[i], activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)
+            )(x)
+        if i < len(layers) - 1:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+    model = K.Model(inputs=inputs, outputs=x)
+    return model
