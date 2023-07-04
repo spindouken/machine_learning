14a034b934453dabe47740455b286c38d27df42d
spindouken <mlc9181209@gmail.com>
2023-07-04 16:04:32 -0500

    ayyy... tfv1 issues..

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
-"""placeholder"""
-import tensorflow.keras as K
-
+"""
+sets up Adam optimization for a keras model
+    with categorical crossentropy loss and accuracy metrics
+"""
+import keras as K
 
 def optimize_model(network, alpha, beta1, beta2):
     """
-    placeholder
+    network is the model to optimize
+    alpha is the learning rate
+    beta1 is the first Adam optimization parameter
+    beta2 is the second Adam optimization parameter
+    Returns: None
     """
-    optimizer = K.optimizers.Adam(learning_rate=alpha,
-                                  beta_1=beta1,
-                                  beta_2=beta2)
-    network.compile(loss='categorical_crossentropy',
-                    optimizer=optimizer,
-                    metrics=["accuracy"])
-    return None
+    optimizer = K.optimizers.Adam(
+        lr=alpha, beta_1=beta1, beta_2=beta2
+        )
+    network.compile(
+        optimizer=optimizer,
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+        )
