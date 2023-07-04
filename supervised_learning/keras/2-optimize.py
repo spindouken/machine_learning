e1e48bacfd905d2548791d69318a4f2e47a2f187
spindouken <mlc9181209@gmail.com>
2023-07-04 15:34:48 -0500

    task 2

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-"""ets up Adam optimization for a keras model with categorical crossentropy loss and accuracy metrics"""
+"""
+sets up Adam optimization for a keras model
+    with categorical crossentropy loss and accuracy metrics
+"""
+import tensorflow.keras as K
 
 
-def build_model(nx, layers, activations, lambtha, keep_prob):
+def optimize_model(network, alpha, beta1, beta2):
     """
     network is the model to optimize
     alpha is the learning rate
@@ -10,4 +14,11 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     beta2 is the second Adam optimization parameter
     Returns: None
     """
-
+    optimizer = K.optimizers.Adam(
+        learning_rate=alpha, beta_1=beta1, beta_2=beta2
+        )
+    network.compile(
+        optimizer=optimizer,
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+        )
