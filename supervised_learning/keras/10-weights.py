b5a55185ebdd51cbcdf82b288ec5523b732ee418
spindouken <mlc9181209@gmail.com>
2023-07-04 18:12:40 -0500

    task 10

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -10,6 +10,8 @@ def save_weights(network, filename, save_format='h5'):
     save_format is the format in which the weights should be saved
     Returns: None
     """
+    network.save_weights(filename, save_format=save_format)
+
 
 def load_weights(network, filename):
     """
@@ -19,4 +21,4 @@ def load_weights(network, filename):
         should be loaded from
     returns none
     """
-
+    network.load_weights(filename)
