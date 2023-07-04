35bb01a6aaeebc2d27e386113b64ea9a35bec1e7
spindouken <mlc9181209@gmail.com>
2023-07-04 15:43:10 -0500

    bug hunt

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -8,11 +8,7 @@ import tensorflow.keras as K
 
 def optimize_model(network, alpha, beta1, beta2):
     """
-    network is the model to optimize
-    alpha is the learning rate
-    beta1 is the first Adam optimization parameter
-    beta2 is the second Adam optimization parameter
-    Returns: None
+    bug?
     """
     optimizer = K.optimizers.Adam(
         learning_rate=alpha, beta_1=beta1, beta_2=beta2
