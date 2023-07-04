62a681f4cae84208804289bac3912b100ff02752
spindouken <mlc9181209@gmail.com>
2023-07-04 15:47:15 -0500

    syntax error?

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -10,12 +10,10 @@ def optimize_model(network, alpha, beta1, beta2):
     """
     bug?
     """
-    optimizer = K.optimizers.Adam(
-        learning_rate=alpha, beta_1=beta1, beta_2=beta2
-        )
-    network.compile(
-        optimizer=optimizer,
-        loss='categorical_crossentropy',
-        metrics=['accuracy']
-        )
+    optimizer = K.optimizers.Adam(learning_rate=alpha,
+                                  beta_1=beta1,
+                                  beta_2=beta2)
+    network.compile(loss='categorical_crossentropy',
+                    optimizer=optimizer,
+                    metrics=["accuracy"])
     return None
