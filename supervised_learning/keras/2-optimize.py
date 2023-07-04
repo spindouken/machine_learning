0203baedf40dab7eaceeed16cce3bde1a6c74a38
spindouken <mlc9181209@gmail.com>
2023-07-04 15:56:15 -0500

    pycodestyle

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -8,8 +8,8 @@ def optimize_model(network, alpha, beta1, beta2):
     placeholder
     """
     optimizer = K.optimizers.Adam(learning_rate=alpha,
-                                       beta_1=beta1,
-                                       beta_2=beta2)
+                                  beta_1=beta1,
+                                  beta_2=beta2)
     network.compile(loss='categorical_crossentropy',
                     optimizer=optimizer,
                     metrics=["accuracy"])
