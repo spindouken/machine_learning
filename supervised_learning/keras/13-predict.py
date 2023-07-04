4ee02dd522385e85d33b9bc15fce643047319d94
spindouken <mlc9181209@gmail.com>
2023-07-04 18:39:19 -0500

    pycodestyle compliance

--- supervised_learning/keras/13-predict.py
+++ supervised_learning/keras/13-predict.py
@@ -7,7 +7,8 @@ def predict(network, data, verbose=False):
     """
     network is the network model to make the prediction with
     data is the input data to make the prediction with
-    verbose is a boolean that determines if output should be printed during the prediction process
+    verbose is a boolean that determines if output should
+        be printed during the prediction process
     Returns: the prediction for the data
     """
     return network.predict(x=data,
