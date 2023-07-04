699b97ac50af0e63db9ff9e1170d4290dcd56143
spindouken <mlc9181209@gmail.com>
2023-07-04 15:53:44 -0500

    syntax edit?...

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-"""
-bug?
-"""
+"""placeholder"""
 import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
     """
-    bug?
+    placeholder
     """
     optimizer = K.optimizers.Adam(learning_rate=alpha,
-                                  beta_1=beta1,
-                                  beta_2=beta2)
+                                       beta_1=beta1,
+                                       beta_2=beta2)
     network.compile(loss='categorical_crossentropy',
                     optimizer=optimizer,
                     metrics=["accuracy"])
