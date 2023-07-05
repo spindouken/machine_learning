4af84d319a1c6abee306e5827c5a8443515cc338
spindouken <mlc9181209@gmail.com>
2023-07-05 15:04:36 -0500

    task 1

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
 """performs a same convolution on grayscale images"""
+import numpy as np
 
 
 def convolve_grayscale_same(images, kernel):
     """
-    images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
-    m is the number of images
-    h is the height in pixels of the images
-    w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
-    kh is the height of the kernel
-    kw is the width of the kernel
+    images is a numpy.ndarray with shape (m, h, w)
+        containing multiple grayscale images
+        m: the number of images
+        h: the height in pixels of the images
+        w: the width in pixels of the images
+    kernel is a numpy.ndarray with shape (kh, kw)
+        containing the kernel for the convolution
+        kh: the height of the kernel
+        kw: the width of the kernel
     if necessary, the image should be padded with 0’s
-    You are only allowed to use two for loops; any other loops of any kind are not allowed
     Returns: a numpy.ndarray containing the convolved images
     """
-    
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+
+    # calculate padding
+    padH = max((kh - 1) // 2, 0)
+    padW = max((kw - 1) // 2, 0)
+
+    # pad dem images
+    padded_images = np.pad(
+        images, ((0, 0), (padH, padH), (padW, padW))
+        )
+
+    convolvedMatrix = np.zeros((m, h, w))
+    for x in range(w):
+        for y in range(h):
+            shredder = padded_images[:, y:y + kh, x:x + kw]
+            convolvedMatrix[:, y, x] = np.tensordot(shredder,
+                                                    kernel,
+                                                    axes=2)
+    return convolvedMatrix
