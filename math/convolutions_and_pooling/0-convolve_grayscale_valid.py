9fdb6a0d4533e255777527bb8f6b9388cb099da9
spindouken <mlc9181209@gmail.com>
2023-07-05 15:02:58 -0500

    task 0

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 """performs a valid convolution on grayscale images"""
+import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
     """
-    images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
-    m is the number of images
-    h is the height in pixels of the images
-    w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
-    kh is the height of the kernel
-    kw is the width of the kernel
-    You are only allowed to use two for loops; any other loops of any kind are not allowed
+    images is a numpy.ndarray with shape (m, h, w)
+        containing multiple grayscale images
+        m: the number of images
+        h: the height in pixels of the images
+        w: the width in pixels of the images
+    kernel is a numpy.ndarray with shape (kh, kw)
+        containing the kernel for the convolution
+        kh: the height of the kernel
+        kw: the width of the kernel
     Returns: a numpy.ndarray containing the convolved images
     """
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+
+    convolvedW = w - kw + 1
+    convolvedH = h - kh + 1
+    convolvedMatrix = np.zeros((m, convolvedH, convolvedW))
+
+    for x in range(convolvedW):
+        for y in range(convolvedH):
+            shredder = images[:, y:y + kh, x:x + kw]
+            convolvedMatrix[:, y, x] = np.tensordot(shredder,
+                                                    kernel,
+                                                    axes=2)
+    return convolvedMatrix
