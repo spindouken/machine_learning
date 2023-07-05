0b6ffd19e37d51005a5783170367d0155e84f7c9
spindouken <mlc9181209@gmail.com>
2023-07-05 16:32:30 -0500

    task 6

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -4,21 +4,44 @@
 
 def pool(images, kernel_shape, stride, mode='max'):
     """
-    images is a numpy.ndarray with shape (m, h, w, c) containing multiple images
-    m is the number of images
-    h is the height in pixels of the images
-    w is the width in pixels of the images
-    c is the number of channels in the image
-    kernel_shape is a tuple of (kh, kw) containing the kernel shape for the pooling
-    kh is the height of the kernel
-    kw is the width of the kernel
+    images is a numpy.ndarray with shape (m, h, w, c)
+        containing multiple images
+        m: the number of images
+        h: the height in pixels of the images
+        w: the width in pixels of the images
+        c: the number of channels in the image
+    kernel_shape is a tuple of (kh, kw)
+        containing the kernel shape for the pooling
+        kh: the height of the kernel
+        kw: the width of the kernel
     stride is a tuple of (sh, sw)
-    sh is the stride for the height of the image
-    sw is the stride for the width of the image
+        sh: the stride for the height of the image
+        sw: the stride for the width of the image
     mode indicates the type of pooling
-    max indicates max pooling
-    avg indicates average pooling
-    You are only allowed to use two for loops; any other loops of any kind are not allowed
+        max: indicates max pooling
+        avg: indicates average pooling
     Returns: a numpy.ndarray containing the pooled images
     """
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+    c = images.shape[3]
+    kh, kw = kernel_shape
+    sh, sw = stride
 
+    if mode == "max":
+        op = np.amax
+    else:
+        op = np.average
+
+    ph, pw = 0, 0
+    pooledW = (w - kw + (2 * pw)) // sw + 1
+    pooledH = (h - kh + (2 * ph)) // sh + 1
+    pooledMatrix = np.zeros((m, pooledH, pooledW, c))
+
+    for i in range(pooledH):
+        for j in range(pooledW):
+            pooledMatrix[:, i, j, :] = op(
+                images[:, sh*i: sh*i+kh, sw*j:sw*j+kw, :], axis=(1, 2)
+                )
+    return pooledMatrix
