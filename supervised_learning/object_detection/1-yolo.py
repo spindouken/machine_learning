d2c15387b7866abb63d20b72e8ee7723f616576b
spindouken <mlc9181209@gmail.com>
2023-08-21 00:25:40 -0500

    error handling

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -82,38 +82,33 @@ class Yolo:
 
         for i, output in enumerate(outputs):
             grid_height, grid_width, anchor_boxes, _ = output.shape
-
-            # Process coordinates
-            tx, ty, tw, th = np.split(output[..., :4], 4, axis=-1)
+            tx = output[..., 0:1]
+            ty = output[..., 1:2]
+            tw = output[..., 2:3]
+            th = output[..., 3:4]
             box_confidence = 1 / (1 + np.exp(-output[..., 4:5]))
             box_class_prob = 1 / (1 + np.exp(-output[..., 5:]))
-
             box_confidences.append(box_confidence)
             box_class_probs.append(box_class_prob)
-
-            pw, ph = self.anchors[i].T
-            bx = (1 / (1 + np.exp(-tx))) + np.arange(grid_width).reshape(
-                1, -1, 1, 1
-            )
-            by = (1 / (1 + np.exp(-ty))) + np.arange(grid_height).reshape(
-                -1, 1, 1, 1
-            )
-            bw = pw * np.exp(tw)
-            bh = ph * np.exp(th)
-
-            # Normalize
-            bx /= grid_width
-            by /= grid_height
-            bw /= int(self.model.input.shape[1])
-            bh /= int(self.model.input.shape[2])
-
-            # Convert to original image scale
-            x1 = (bx - (bw / 2)) * image_size[1]
-            y1 = (by - (bh / 2)) * image_size[0]
-            x2 = (bx + (bw / 2)) * image_size[1]
-            y2 = (by + (bh / 2)) * image_size[0]
-
-            # Concatenate to final box coordinates
-            boxes.append(np.concatenate((x1, y1, x2, y2), axis=-1))
-
+            for cy in range(grid_height):
+                for cx in range(grid_width):
+                    for b in range(anchor_boxes):
+                        pw, ph = self.anchors[i][b]
+                        bx = (1 / (1 + np.exp(-tx[cy, cx, b]))) + cx
+                        by = (1 / (1 + np.exp(-ty[cy, cx, b]))) + cy
+                        bw = pw * np.exp(tw[cy, cx, b])
+                        bh = ph * np.exp(th[cy, cx, b])
+                        bx /= grid_width
+                        by /= grid_height
+                        bw /= int(self.model.input.shape[1])
+                        bh /= int(self.model.input.shape[2])
+                        x1 = (bx - (bw / 2)) * image_size[1]
+                        y1 = (by - (bh / 2)) * image_size[0]
+                        x2 = (bx + (bw / 2)) * image_size[1]
+                        y2 = (by + (bh / 2)) * image_size[0]
+                        tx[cy, cx, b] = x1
+                        ty[cy, cx, b] = y1
+                        tw[cy, cx, b] = x2
+                        th[cy, cx, b] = y2
+            boxes.append(np.concatenate((tx, ty, tw, th), axis=-1))
         return (boxes, box_confidences, box_class_probs)
