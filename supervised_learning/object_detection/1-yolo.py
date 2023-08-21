789eab52bb1c20e32652427453ef108c686d052e
spindouken <mlc9181209@gmail.com>
2023-08-21 00:11:34 -0500

    mega y test

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-"""
-uses the Yolo v3 algorithm to perform object detection
-"""
+"""Placeholder"""
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 import numpy as np
-import tensorflow as tf
 
 
 class Yolo:
+    """
+    Placeholder
+    """
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """
-        placeholder
-        """
         self.model = tf.keras.models.load_model(model_path)
         with open(classes_path, "r") as f:
             self.class_names = [line.strip() for line in f.readlines()]
@@ -20,38 +19,40 @@ class Yolo:
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
-        """
-        placeholder
-        """
+        """Placeholder"""
         boxes = []
         box_confidences = []
         box_class_probs = []
-        for i in range(len(outputs)):
-            boxes.append(outputs[i][..., :4])
-            box_confidences.append(1 / (1 + np.exp(-outputs[i][..., 4:5])))
-            box_class_probs.append(1 / (1 + np.exp(-outputs[i][..., 5:])))
-        image_height, image_width = image_size
-        for i in range(len(boxes)):
-            grid_width = outputs[i].shape[1]
-            grid_height = outputs[i].shape[0]
-            anchor_boxes = outputs[i].shape[2]
+
+        for i, output in enumerate(outputs):
+            grid_height, grid_width, anchor_boxes, _ = output.shape
+            tx = output[..., 0:1]
+            ty = output[..., 1:2]
+            tw = output[..., 2:3]
+            th = output[..., 3:4]
+            box_confidence = 1 / (1 + np.exp(-output[..., 4:5]))
+            box_class_prob = 1 / (1 + np.exp(-output[..., 5:]))
+            box_confidences.append(box_confidence)
+            box_class_probs.append(box_class_prob)
             for cy in range(grid_height):
                 for cx in range(grid_width):
                     for b in range(anchor_boxes):
-                        tx, ty, tw, th = boxes[i][cy, cx, b]
                         pw, ph = self.anchors[i][b]
-                        bx = (1 / (1 + np.exp(-tx))) + cx
-                        by = (1 / (1 + np.exp(-ty))) + cy
-                        bw = pw * np.exp(tw)
-                        bh = ph * np.exp(th)
+                        bx = (1 / (1 + np.exp(-tx[cy, cx, b]))) + cx
+                        by = (1 / (1 + np.exp(-ty[cy, cx, b]))) + cy
+                        bw = pw * np.exp(tw[cy, cx, b])
+                        bh = ph * np.exp(th[cy, cx, b])
                         bx /= grid_width
                         by /= grid_height
-                        bw /= self.model.input.shape[1]
-                        bh /= self.model.input.shape[2]
-                        x1 = (bx - (bw / 2)) * image_width
-                        y1 = (by - (bh / 2)) * image_height
-                        x2 = (bx + (bw / 2)) * image_width
-                        y2 = (by + (bh / 2)) * image_height
-                        boxes[i][cy, cx, b] = [x1, y1, x2, y2]
-
-        return boxes, box_confidences, box_class_probs
+                        bw /= int(self.model.input.shape[1])
+                        bh /= int(self.model.input.shape[2])
+                        x1 = (bx - (bw / 2)) * image_size[1]
+                        y1 = (by - (bh / 2)) * image_size[0]
+                        x2 = (bx + (bw / 2)) * image_size[1]
+                        y2 = (by + (bh / 2)) * image_size[0]
+                        tx[cy, cx, b] = x1
+                        ty[cy, cx, b] = y1
+                        tw[cy, cx, b] = x2
+                        th[cy, cx, b] = y2
+            boxes.append(np.concatenate((tx, ty, tw, th), axis=-1))
+        return (boxes, box_confidences, box_class_probs)
