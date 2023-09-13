b5a4a1ab6601494562e431d2c91ff0fbee9162a7
spindouken <mlc9181209@gmail.com>
2023-09-13 03:16:05 -0500

    task 0 protofile

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -14,7 +14,7 @@ def likelihood(x, n, P):
     """
     Calculates the likelihood of obtaining this data given various
         hypothetical probabilities of developing severe side effects
-    
+
     x: number of patients that develop severe side effects
     n: total number of patients observed
     P: 1D numpy.ndarray containing the various hypothetical probabilities
@@ -34,3 +34,15 @@ def likelihood(x, n, P):
     Returns: a 1D numpy.ndarray containing the likelihood of obtaining the
         data, x and n, for each probability in P, respectively
     """
+    if n < 0:
+        raise ValueError('n must be a positive integer')
+    if x < 0:
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0'
+        )
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if type(P) is not np.ndarray:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError('All values in P must be in the range [0, 1]')
