c576373394f88f92e06feb1d0d42ddc1f2863dcb
spindouken <mlc9181209@gmail.com>
2023-09-26 03:56:36 -0500

    task 12 set-up

---
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+performs agglomerative clustering on a dataset
+"""
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """
+    X is a numpy.ndarray of shape (n, d) containing the dataset
+    dist is the maximum cophenetic distance for all clusters
+
+    Performs agglomerative clustering with Ward linkage
+    Displays the dendrogram with each cluster displayed in a different color
+
+    Returns: clss, a numpy.ndarray of shape (n,) containing the cluster
+        indices for each data point
+    """
