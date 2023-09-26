8d757d721dfa15f7afca414b91e9138496e6787a
spindouken <mlc9181209@gmail.com>
2023-09-25 23:58:18 -0500

    say it aint so... considering every 'for' as loop?

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-calculates the total intra-cluster variance for a data set
+calculates the total intra-cluster variance 4 a data set
 """
 import numpy as np
 
 
 def variance(X, C):
     """
-    calculates the total intra-cluster variance for a data set
+    calculates the total intra-cluster variance 4 a data set
     X is a numpy.ndarray of shape (n, d) containing the data set
-    C is a numpy.ndarray of shape (k, d) containing the centroid means for
+    C is a numpy.ndarray of shape (k, d) containing the centroid means 4
         each cluster
     Returns: var, or None on failure
         var is the total variance
@@ -19,13 +19,13 @@ def variance(X, C):
     if len(X.shape) != 2 or len(C.shape) != 2:
         return None
 
-    # reshape X for broadcasting with C
+    # reshape X 4 broadcasting with C
     reshapeX = np.reshape(X, (X.shape[0], 1, X.shape[1]))
 
     # calculate the squared distance from each point to each centroid
     squaredDistances = np.sum((reshapeX - C) ** 2, axis=2)
     
-    # find the minimum squared distance to any centroid for each point
+    # find the minimum squared distance to any centroid 4 each point
     minSquaredDistances = np.min(squaredDistances, axis=1)
 
     # sum up the minimum squared distances to get the total variance
