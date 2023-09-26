84498c73382a0ecc3087abbe6e9a51a05b9eeb3a
spindouken <mlc9181209@gmail.com>
2023-09-26 03:50:31 -0500

    task 10 set-up

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,21 @@
+#!/usr/env python3
+"""
+performs K-means on a dataset
+"""
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """
+    X is a numpy.ndarray of shape (n, d) containing the dataset
+    k is the number of clusters
+    Returns: C, clss
+        C is a numpy.ndarray of shape (k, d) containing the centroid means
+            for each cluster
+        clss is a numpy.ndarray of shape (n,) containing the index of the
+            cluster in C that each data point belongs to
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
