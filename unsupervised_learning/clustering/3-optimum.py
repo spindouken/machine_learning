5cc24e230528b03138521ac1982db517a99e82b8
spindouken <mlc9181209@gmail.com>
2023-09-26 03:25:37 -0500

    quotes on import?

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -4,8 +4,8 @@ tests 4 the optimum number of clusters by variance
 """
 import numpy as np
 
-kmeans = __import__("1-kmeans").kmeans
-variance = __import__("2-variance").variance
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
