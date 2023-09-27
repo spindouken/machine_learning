d78ed01e6ad97f78149d4780929598604c3d0c73
spindouken <mlc9181209@gmail.com>
2023-09-27 01:04:28 -0500

    compnonents retrieval and matrix formatting edit

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -8,6 +8,33 @@ import numpy as np
 def pca(X, var=0.95):
     """
     X is a numpy.ndarray of shape (n, d) where:
+        n is the number of data points
+        d is the number of dimensions in each point
+            all dimensions have a mean of 0 across all data points
+    var is the fraction of the variance that the PCA transformation should
+        maintain
+
+    variables:
+    U, S, Vt: result of SVD on centeredData
+        S' contains the singular values, indicating
+            the importance of corresponding vectors in Vt
+    totalVariance: sum of the squares of all singular values
+        represents total data variance
+    retainedVariance: cccumulates the variance
+        as we consider each singular value in 'S'
+    numComponents: Counts the number of singular values
+        (principal components) needed to achieve the desired 'var'
+        Used to construct 'W'
+
+    result of singular value decomposition (SVD) on mean centered data:
+        U: left singular vectors
+        S: singular values
+        Vt: right singular vectors
+
+    Returns: the weights matrix, W, that maintains var fraction of X‘s
+        original variance
+        W is a numpy.ndarray of shape (d, nd)
+            where nd is the new dimensionality of the transformed X
     """
     # singular value decomposition (SVD) on mean centered data
     U, S, Vt = np.linalg.svd(X, full_matrices=False)
@@ -15,16 +42,17 @@ def pca(X, var=0.95):
     # summing up the squares of singular values to get total variance
     totalVariance = np.sum(S**2)
 
-    # calculate the retained variance for each component
-    retainedVariance = np.cumsum(S**2) / totalVariance
+    retainedVariance = 0
+    numComponents = 0
 
-    # find the number of components needed to retain the given variance
-    numComponents = np.argmax(retainedVariance >= var) + 1
+    # loop through each singular value in 'S'
+    for singularValue in S:
+        retainedVariance += (singularValue**2) / totalVariance
+        numComponents += 1
+        # stop when we hit the required variance
+        if retainedVariance >= var:
+            break
 
-    # grab the top 'numComponents' columns from
-    #   Vt's transpose for our weight matrix W
-    # these columns capture the most significant
-    #   'directions' of variance in our dataset
-    W = Vt.T[:, :numComponents]
+    W = Vt[: numComponents + 1].T
 
     return W
