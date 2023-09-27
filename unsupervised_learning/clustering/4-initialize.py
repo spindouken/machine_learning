ec6658d4a86e6a96972522eb4f06f1ea84a88009
spindouken <mlc9181209@gmail.com>
2023-09-27 17:50:56 -0500

    task 4 initialize GMM

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -3,7 +3,8 @@
 initializes variables for a Gaussian Mixture Model
 """
 import numpy as np
-kmeans = __import__('1-kmeans').kmeans
+
+kmeans = __import__("1-kmeans").kmeans
 
 
 def initialize(X, k):
@@ -21,5 +22,21 @@ def initialize(X, k):
     """
     if not isinstance(X, np.ndarray) or not isinstance(k, int):
         return None, None, None
-    if k < 1 or len(X.shape) != 2:
+    if k < 1 or len(X.shape) != 2 or X.shape[0] < k:
         return None, None, None
+
+    # initialize uniform prior probabilities for each cluster
+    pi = np.ones(k) / k
+
+    # get centroid means using kmeans... ignore cluster lables (clss)
+    m, _ = kmeans(X, k)  # m will have shape (k, d)
+
+    # pull number of dimensions from X
+    d = X.shape[1]
+
+    # initialize identity covariance matrices for each cluster
+    S = np.identity(d)  # single identity matrix of shape (d, d)
+    # repeat identity matrix k times to get shape (k, d, d)
+    S = np.repeat(S[np.newaxis, :, :], k, axis=0)
+
+    return pi, m, S
