04739b7cc1268f37f4fce3fcfd6dcc08262b0539
spindouken <mlc9181209@gmail.com>
2023-09-27 01:07:43 -0500

    docstring limit testing

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -8,6 +8,27 @@ import numpy as np
 def pca(X, var=0.95):
     """
     X is a numpy.ndarray of shape (n, d) where:
+        n is the number of data points
+        d is the number of dimensions in each point
+            all dimensions have a mean of 0 across all data points
+    var is the fraction of the variance that the PCA transformation should
+        maintain
+
+    U, S, Vt: result of SVD on centeredData
+        S' contains the singular values, indicating
+            the importance of corresponding vectors in Vt
+    totalVariance: sum of the squares of all singular values
+        represents total data variance
+    retainedVariance: cccumulates the variance
+        as we consider each singular value in 'S'
+    numComponents: Counts the number of singular values
+        (principal components) needed to achieve the desired 'var'
+        Used to construct 'W'
+
+    Returns: the weights matrix, W, that maintains var fraction of X‘s
+        original variance
+        W is a numpy.ndarray of shape (d, nd)
+            where nd is the new dimensionality of the transformed X
     """
     # singular value decomposition (SVD) on mean centered data
     U, S, Vt = np.linalg.svd(X, full_matrices=False)
