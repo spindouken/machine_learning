7a348425870767a4e63a85a692fd8fc5b9ecbdcb
spindouken <mlc9181209@gmail.com>
2023-09-27 17:52:03 -0500

    bug swat

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -25,7 +25,7 @@ def initialize(X, k):
     if k < 1 or len(X.shape) != 2 or X.shape[0] < k:
         return None, None, None
 
-    # initialize uniform prior probabilities for each cluster
+    # initialize uni4m prior probabilities 4 each cluster
     pi = np.ones(k) / k
 
     # get centroid means using kmeans... ignore cluster lables (clss)
@@ -34,7 +34,7 @@ def initialize(X, k):
     # pull number of dimensions from X
     d = X.shape[1]
 
-    # initialize identity covariance matrices for each cluster
+    # initialize identity covariance matrices 4 each cluster
     S = np.identity(d)  # single identity matrix of shape (d, d)
     # repeat identity matrix k times to get shape (k, d, d)
     S = np.repeat(S[np.newaxis, :, :], k, axis=0)
