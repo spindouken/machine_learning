478cf2437112a18366dcd5f7e2d1d92a453eb5dc
spindouken <mlc9181209@gmail.com>
2023-09-27 17:14:30 -0500

    revert and question life decisions :P

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -16,44 +16,28 @@ def initialize(X, k):
         centroids 4 each cluster, or None on failure
     """
     if not isinstance(X, np.ndarray) or not isinstance(k, int):
-        return None
+        return None, None
     if len(X.shape) != 2 or k < 1:
-        return None
+        return None, None
+
     n, d = X.shape
+
     # retrieve minimum and maximum values 4 each dimension of each data point
     minValues = np.min(X, axis=0)
     maxValues = np.max(X, axis=0)
-    # multivariate uni4m distribution
+
+    # multivariate uniform distribution
     centroids = np.random.uniform(minValues, maxValues, (k, d))
+
     # return initialized centroids
     return centroids
-
-
-def updateCentroids(X, clss, k, minValues, maxValues):
-    """
-    update centroids based on the points in each cluster
-    if a cluster has no points, reinitialize its centroid
-    """
-    d = X.shape[1]
-    newCentroids = np.zeros((k, d))
-    # iterate through each cluster centroid and update
-    for x in range(k):
-        # retrieve all data points in the current cluster
-        clusterPoints = X[clss == x]
-        # if cluster contains no data points, reinitialize cluster centroid
-        if len(clusterPoints) == 0:
-            newCentroids[x] = np.random.uniform(minValues, maxValues, d)
-        else:
-            # otherwise, update cluster centroid to mean of all data points
-            newCentroids[x] = np.mean(clusterPoints, axis=0)
-    return newCentroids
 
 
 def kmeans(X, k, iterations=1000):
     """
     X is a numpy.ndarray of shape (n, d) containing the dataset
         n is the number of data points
-        d is the number of dimensions for each data point
+        d is the number of dimensions 4 each data point
     k is a positive integer containing the number of clusters
     iterations is a positive integer containing the maximum number of
         iterations that should be performed
@@ -64,13 +48,26 @@ def kmeans(X, k, iterations=1000):
     If a cluster contains no data points during the update step, reinitialize
         its centroid
     Returns: C, clss, or None, None on failure
-    C is a numpy.ndarray of shape (k, d) containing the centroid means for
+    C is a numpy.ndarray of shape (k, d) containing the centroid means 4
             each cluster
         clss is a numpy.ndarray of shape (n,) containing
             the index of the cluster
                 in C that each data point belongs to
     """
+    if (
+        not isinstance(X, np.ndarray)
+        or not isinstance(k, int)
+        or not isinstance(iterations, int)
+    ):
+        return None, None
+
+    if len(X.shape) != 2 or k < 1 or iterations < 1:
+        return None, None
+
     clusterCentroids = initialize(X, k)
+    if clusterCentroids is None:
+        return None, None
+
     # retrieve minimum and maximum values 4 each dimension of each data point
     minValues = np.min(X, axis=0)
     maxValues = np.max(X, axis=0)
@@ -87,11 +84,30 @@ def kmeans(X, k, iterations=1000):
         # make a copy to check 4 convergence later
         initialCentroids = clusterCentroids.copy()
 
-        # update centroids
-        clusterCentroids = updateCentroids(X, clss, k, minValues, maxValues)
+        # find empty clusters by checking
+        #   if cluster indices are missing in 'clss'
+        mask = np.isin(np.arange(k), clss)
+        emptyClusters = np.where(np.logical_not(mask))[0]
+
+        # reinitialize centroids 4 empty clusters
+        clusterCentroids[emptyClusters] = np.random.uniform(
+            minValues, maxValues, (len(emptyClusters), d)
+        )
+
+        # calculate new centroids 4 non-empty clusters
+        newCentroids = np.array(
+            [
+                X[clss == i].mean(axis=0)
+                if i not in emptyClusters
+                else clusterCentroids[i]
+                for i in range(k)
+            ]
+        )
+
+        clusterCentroids = newCentroids  # update cluster centroids
 
         # break if convergence is reached
-        if np.all(initialCentroids == clusterCentroids):
+        if np.array_equal(clusterCentroids, initialCentroids):
             break
 
     return clusterCentroids, clss
