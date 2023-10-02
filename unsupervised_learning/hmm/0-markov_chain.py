09da8cc7b4d78927da5a78f9ed804c7b83944771
spindouken <mlc9181209@gmail.com>
2023-10-02 16:45:47 -0500

    task 0 markov chain

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -3,7 +3,6 @@
 determines the probability of a markov chain being in a particular state
     after a specified number of iterations
 """
-import numpy as np
 
 
 def markov_chain(P, s, t=1):
@@ -18,3 +17,6 @@ def markov_chain(P, s, t=1):
     Returns: a numpy.ndarray of shape (1, n) representing the probability of
         being in a specific state after t iterations, or None on failure
     """
+    for x in range(t):
+        s = s @ P
+    return s
