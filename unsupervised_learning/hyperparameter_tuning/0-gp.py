8de15b58863954772a48138f22edead410e71bc0
spindouken <mlc9181209@gmail.com>
2023-10-06 14:33:13 -0500

    setting K at kernel of X with itself

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -29,6 +29,7 @@ class GaussianProcess:
         self.Y = Y_init
         self.l = l
         self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
         """
@@ -39,5 +40,3 @@ class GaussianProcess:
         Returns: the covariance kernel matrix as a numpy.ndarray
             of shape (m, n)
         """
-        X1 = np.array(X1)
-        X2 = np.array(X2)
