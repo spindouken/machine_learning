d93679c6b99ab40f51cb9532d38e355b1388e380
spindouken <mlc9181209@gmail.com>
2023-10-06 13:06:55 -0500

    checker likes single quotes xD

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -4,7 +4,7 @@ create the class BayesianOptimization that performs Bayesian optimization
     on a noiseless 1D Gaussian process
 """
 import numpy as np
-GP = __import__("2-gp").GaussianProcess
+GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
