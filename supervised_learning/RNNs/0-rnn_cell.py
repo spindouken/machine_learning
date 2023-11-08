3592de1e0ce6c88dee4d88e0e7fbf625ebd04e8c
spindouken <mlc9181209@gmail.com>
2023-11-07 19:23:03 -0600

    simple rnn cell

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -5,11 +5,18 @@ creates a class RNNCell that represents a cell of a simple RNN
 import numpy as np
 
 
+def softmax(X):
+    """
+    softmax function
+    """
+    exponentiatedX = np.exp(X - np.max(X, axis=1, keepdims=True))
+    return exponentiatedX / exponentiatedX.sum(axis=1, keepdims=True)
+
+
 class RNNCell:
     """
     represents a cell of a simple RNN
     """
-
     def __init__(self, i, h, o):
         """
         i is the dimensionality of the data
@@ -24,10 +31,16 @@ class RNNCell:
         The weights will be used on the right side for matrix multiplication
         The biases should be initialized as zeros
         """
-        self.Wh =
-        self.bh =
-        self.Wy =
-        self.by =
+        # i + h = h_prev + x_t
+        #     = total number of features input in RNNCell at each time step
+        #     (includes input features and features from prev hidden state)
+        #     (in a simple RNN, input at step
+        #       is current data point concat w/ prev data point)
+        # i + h rows and h columns
+        self.Wh = np.random.normal(size=(i + h, h))
+        self.bh = np.zeros(shape=(1, h))
+        self.Wy = np.random.normal(size=(h, o))
+        self.by = np.zeros(shape=(1, o))
 
     def forward(self, h_prev, x_t):
         """
@@ -41,3 +54,24 @@ class RNNCell:
             h_next is the next hidden state
             y is the output of the cell
         """
+        # concatenate previous hidden state and current input
+        # why? an RNN cell considers both the new input
+        #   and the past context when making predictions
+        inputAndPrevState_combined = np.concatenate((h_prev, x_t), axis=1)
+
+        # calc next hidden state by multiplying inputAndPrevState_combined by
+        #   weight matrix for hidden state and adding bias
+        #   use hyperbolic tangent (tanh) activation func to scale to [-1, 1]
+        h_next = np.tanh(inputAndPrevState_combined @ self.Wh + self.bh)
+
+        # use next hidden state calculation (h_next) to compute output of cell
+        #   by multiplying h_next by weight matrix for output and adding bias
+        unactivatedOutput = h_next @ self.Wy + self.by
+
+        # softmax for classification (activated output)
+        #   (returns a vector of probabilities summing up to 1)
+        y = softmax(unactivatedOutput)
+
+        # h_next used for subsequent time steps
+        # y is output of the cell (current prediction)
+        return h_next, y
