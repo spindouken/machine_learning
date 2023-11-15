ec4d57fb290a0bf65f6c9ed9535f078382438ce9
spindouken <mlc9181209@gmail.com>
2023-11-15 15:32:12 -0600

    pycodestyle compliance

--- supervised_learning/RNNs/7-bi_output.py
+++ supervised_learning/RNNs/7-bi_output.py
@@ -70,8 +70,10 @@ class BidirectionalCell:
     def output(self, H):
         """
         calculates all outputs for the RNN
-        H is a numpy.ndarray of shape (t, m, 2 * h) that contains the concatenated
-            hidden states from both directions, excluding their initialized states
+        H is a numpy.ndarray of shape (t, m, 2 * h)
+            that contains the concatenated
+            hidden states from both directions,
+                excluding their initialized states
             t is the number of time steps
             m is the batch size for the data
             h is the dimensionality of the hidden states
