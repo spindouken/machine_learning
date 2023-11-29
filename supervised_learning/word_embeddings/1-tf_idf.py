85fd3dc8b139d35c700afe8b1a4255a2169724d1
spindouken <mlc9181209@gmail.com>
2023-11-29 14:12:56 -0600

    task 1: create tf-idf embedding

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -3,6 +3,7 @@
 creates a tf-idf embedding
 """
 import numpy as np
+import string
 
 
 def tf_idf(sentences, vocab=None):
@@ -18,3 +19,49 @@ def tf_idf(sentences, vocab=None):
             f is the number of features analyzed
         features is a list of the features used for embeddings
     """
+    # normalize sentences: lowercase, remove punctuation,
+    #   and handle possessives before splitting the sentences into words
+    normalizedSentences = [
+        sentence.lower()
+        .replace("'s", "")
+        .translate(str.maketrans("", "", string.punctuation))
+        for sentence in sentences
+    ]
+
+    # use all words in the sentences if vocab is not provided
+    if vocab is None:
+        # Create a set of unique words from all sentences if vocab not provided
+        features = set(
+            word for sentence in normalizedSentences
+            for word in sentence.split()
+        )
+    else:
+        features = vocab  # if vocab is not None, set it as the features
+
+    # initialize tf (term frequency) matrix
+    tf = []
+
+    # loop through each normalized sentence
+    for sentence in normalizedSentences:
+        # split the sentence into individual words
+        sentenceWords = sentence.split()
+
+        # calculate term frequency
+        sentenceTF = [
+            sentenceWords.count(word) / len(sentenceWords) for word in features
+        ]
+        tf.append(sentenceTF)
+
+    tf = np.array(tf)
+
+    # calculate inverse document frequency
+    idf = np.log(len(sentences) / (1 + np.sum(tf > 0, axis=0))) + 1
+
+    # calculate tf-idf
+    embeddings = tf * idf
+
+    # normalize tf-idf embeddings
+    embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
+    embeddings = np.nan_to_num(embeddings)  # replace NaNs with zeros
+
+    return embeddings, features
