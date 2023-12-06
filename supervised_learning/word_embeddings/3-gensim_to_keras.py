172c080a24af6e676dc1da841b7b801e5d630910
spindouken <mlc9181209@gmail.com>
2023-12-05 21:26:33 -0600

    task 3 -conv gensim word2vec model2keras embedding

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -11,3 +11,21 @@ def gensim_to_keras(model):
     model is a trained gensim word2vec models
     Returns: the trainable keras Embedding
     """
+    import tensorflow.keras as K
+    from gensim.models import Word2Vec
+
+    if not isinstance(model, Word2Vec):
+        raise ValueError("input must be a trained gensim word2vec model")
+
+    # extract embeddings from trained input gensim word2vec model
+    embeddings = model.wv.vectors
+
+    # extract the number of tokens and the dimensionality of the embeddings
+    #   from the embeddings
+    numTokens, embeddingDim = embeddings.shape
+
+    # create and return trainable keras embedding layer
+    return K.layers.Embedding(input_dim=numTokens,
+                              output_dim=embeddingDim,
+                              weights=[embeddings],
+                              trainable=True)
